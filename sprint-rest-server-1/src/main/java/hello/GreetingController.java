d3568f194aa6b57737d4d75afacb826e2840f393
BOAZWIN10\boaz <boaz>
2018-06-17 00:22:19 +0300

    Allow cors for all

--- sprint-rest-server-1/src/main/java/hello/GreetingController.java
+++ sprint-rest-server-1/src/main/java/hello/GreetingController.java
@@ -14,7 +14,9 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     //https://developer.okta.com/blog/2017/12/06/bootiful-development-with-spring-boot-and-react
-    @CrossOrigin(origins = "http://localhost:3000")
+    //@CrossOrigin(origins = "http://localhost:3000")
+    // or to all  https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
+    @CrossOrigin
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
