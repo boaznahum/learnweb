cf369a4cbc9188fe7997c86d5d871f77f454dc52
Boaz Nahum <Boaz_Nahum@amat.com>
2018-06-18 16:44:37 +0100

    client now with borders and all page

--- git-training-client/src/App.tsx
+++ git-training-client/src/App.tsx
@@ -1,25 +1,50 @@
 import * as React from 'react';
 import './App.css';
+// @ts-ignore
 import Greeting from './Greeting'
 
-import logo from './logo.svg';
-
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Type Script</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    public render() {
+        return (
 
-        <Greeting hasConsole={false}/>
-      </div>
-    );
-  }
+
+            <table className="App">
+                <tr>
+                    <td className="Remote" colSpan={2}>
+                        <h3>Remote</h3>
+
+
+                    </td>
+                </tr>
+                <tr>
+                    <td className="Local">
+                        <h3>Local 1</h3>
+
+                    </td>
+
+                    <td className="Local">
+                        <h3>Local 2</h3>
+
+                    </td>
+                </tr>
+
+            </table>
+
+            /*
+                    <div className="App">
+                    <header className="App-header">
+                      <img src={logo} className="App-logo" alt="logo" />
+                      <h1 className="App-title">Welcome to Type Script</h1>
+                    </header>
+                    <p className="App-intro">
+                      To get started, edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+
+                    <Greeting hasConsole={false}/>
+                  </div>
+            */
+        );
+    }
 }
 
 export default App;
