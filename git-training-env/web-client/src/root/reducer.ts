1afce4791a6aabddfbf57c29f57ea2b394740a02
Rebecca Templeman <Rivka_Tempelman@amat.com>
2018-07-11 13:07:46 +0300

    connected Terminal to Redux

--- git-training-env/web-client/src/root/reducer.ts
+++ git-training-env/web-client/src/root/reducer.ts
@@ -3,6 +3,7 @@
 //
 import {Action, combineReducers} from "redux";
 
+import {ActionTypes, RepoID, SetCurrentRepoAction} from "../repository/Actions";
 import * as Repo from '../repository/Reducer'
 
 
@@ -11,12 +12,15 @@ export interface RootState {
     someDummy:string;
 
     repo:Repo.RepositoriesState
+
+    currentRepo:RepoID
 }
 
 export const initialState:RootState = {
 
     someDummy:"A dummy to learn",
-    repo:Repo.repositoriesInitial
+    repo:Repo.repositoriesInitial,
+    currentRepo:RepoID.LOCAL1
 
 };
 
@@ -25,6 +29,16 @@ export function dummy(state: string = "kuku", action: Action) {
     return state;
 
 
+}
+
+function currentRepoReducer(currentRepo:RepoID=RepoID.LOCAL1, action: Action):RepoID{
+
+    if(action.type===ActionTypes.SET_CURRENT_REPO){
+        const myAction:SetCurrentRepoAction = action as SetCurrentRepoAction;
+        return myAction.payload.currentRepo;
+
+    }
+    return currentRepo;
 }
 
 /**
@@ -38,7 +52,8 @@ export const  reducer = combineReducers<RootState>(
     {
         // what does this name mean ? - this is the name in the super state !!!
         someDummy:dummy,
-        repo:Repo.repositoriesReducer
+        repo:Repo.repositoriesReducer,
+        currentRepo:currentRepoReducer
     }
 );
 
